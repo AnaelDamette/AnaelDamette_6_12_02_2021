342be9da2ff7a0acaefdea3bcfb0eb133e70ba39
Anael Damette <damette.anael@gmail.com>
2021-02-17 07:51:03 +0100

    Gestion sauce sans like

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce.js');
 const userRoutes = require('./routes/user.js');
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb+srv://Net:pourquoi@testbasededonne.ercrp.mongodb.net/test?retryWrites=true&w=majority',
@@ -15,15 +17,20 @@ mongoose.connect('mongodb+srv://Net:pourquoi@testbasededonne.ercrp.mongodb.net/t
     console.log(error)
   });
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
+
 
 module.exports = app;
